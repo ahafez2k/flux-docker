f6db2f4c4fd23c8bac875d832d1160027ca0a8c1
Ahmed Hafez <a.hafez.elshabasy@gmail.com>
2025-08-09 21:06:17 +0300

    last changes

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, Ahmed from DockerHub'
+    return 'My name is Ahmed Elshabasy, iam Watchtour'
 
 # Build the database URI from environment variables
 db_user = os.getenv('DB_USER')
